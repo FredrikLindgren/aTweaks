0c5531cbc6078328305e0acdd6bb50da15d020d8
FredrikLindgren <lindgren.fredrik@gmail.com>
2012-06-02 16:40:48 +0200

    Remove tags stuff from vc and fix a few bugs that snuck into PnP Fiends

--- tags.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-find . -regex '.*\.\(tp.\|ssl\|slb\)' | xargs etags -l none --regex='/[ \t]*Name(\([A-Za-z0-9]+\))/\1/' --regex='/[ \t]*\(TRIGGER\|TARGET\)=\([A-Za-z0-9]+\)/\2/' --regex='/[ \t]*DEFINE_\(ACTION\|PATCH\)_\(MACRO\|FUNCTION\)[ \t]+\([A-Za-z0-9#_]+\)/\3/'
